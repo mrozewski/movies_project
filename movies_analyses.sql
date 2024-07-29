3f50470b34679160e3c8468e8b458967253be5cb
mrozewski <155000607+mrozewski@users.noreply.github.com>
2024-07-29 12:05:23 -0400

    Update movies_analyses.sql
    Fixing indents

--- movies_analyses.sql
+++ movies_analyses.sql
@@ -5,8 +5,8 @@
 SELECT d.director_name, STRING_AGG(m.series_title, ', ') AS titles, 
 	STRING_AGG(m.genre, ', ') AS genres, 
 	AVG(m.imdb_rating) AS avg_imdb_rating, 
-    AVG(m.meta_score) AS avg_meta_score, 
-    SUM(m.gross) AS total_gross
+	AVG(m.meta_score) AS avg_meta_score, 	
+	SUM(m.gross) AS total_gross
 FROM directors as d
 JOIN movies as m
 ON d.director_name= m.director
@@ -18,9 +18,9 @@ ORDER BY AVG(m.meta_score) DESC;
 --(Cast float columns as numeric)
 
 SELECT genre,  COUNT(series_title) AS number_movies,
-    ROUND(AVG(gross), 0) AS avg_gross, 
-    ROUND(AVG(budget_x), 0) AS avg_budget, 
-    ROUND(AVG(runtime), 0) AS avg_runtime,
+	ROUND(AVG(gross), 0) AS avg_gross, 
+	ROUND(AVG(budget_x), 0) AS avg_budget, 
+	ROUND(AVG(runtime), 0) AS avg_runtime,
 	ROUND(CAST(AVG(imdb_rating) AS NUMERIC), 1) AS avg_imdb_rating,
 	ROUND(CAST(AVG(meta_score) AS NUMERIC), 1) AS avg_meta_score
 FROM movies
@@ -30,8 +30,10 @@ HAVING count(series_title) > 5
 --Rating Analysis
 --For the common movie ratings (G, PG, PG-13, R), find the average stats
 
-SELECT certificate as rating, COUNT(series_title) as titles, ROUND(AVG(runtime), 0) as avg_runtime, 
-	ROUND(AVG(gross), 0) as avg_gross, ROUND(AVG(CAST(imdb_rating AS NUMERIC)), 2) AS avg_imdb_rating,
+SELECT certificate as rating, COUNT(series_title) as titles, 
+	ROUND(AVG(runtime), 0) as avg_runtime, 
+	ROUND(AVG(gross), 0) as avg_gross, 
+	ROUND(AVG(CAST(imdb_rating AS NUMERIC)), 2) AS avg_imdb_rating,
 	ROUND(AVG(CAST(meta_score AS NUMERIC)), 2) as avg_meta_score
 FROM movies
 WHERE certificate in ('G','PG','PG-13','R')
